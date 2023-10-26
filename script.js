9886b581ff74a5b7e89db1390616cf2363f5bc48
staargaze.r <90391654+starryerik@users.noreply.github.com>
2023-10-26 17:24:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
-function silco(){
-    var respostaCerta = "correta";
-    var respostaUsuario = document.querySelector('input[name="resposta"]:checked');
-
-    if(respostaUsuario){
-        if(respostaUsuario.value == respostaCerta){
-            document.getElementById("retorno").innerHTML = "BIIIOGOOO IGOBIGOBIGO";
-        }
-        else{
-            document.getElementById("retorno").innerHTML = "UUGIGIGIOOOO BIOOGOOOO :(";
-        }
+function inclui_nome() {
+    var nome_variavel = prompt("Coloque seu nome:");
+    if(nome_variavel){
+        document.getElementById("marcacao_nome").innerHTML = nome_variavel;
     }
     else{
-        alert("está contente com as escolhas que anda fazendo nessa sua encarnação?");
+        alert("Nome não atribuido, preencha o nome.");
+        inclui_nome();
     }
+}
+
+function funcao_resposta(){
+
+    var resp1 = document.querySelector('input[name="quest_1"]:checked');
+    var resp2 = document.querySelector('input[name="quest_2"]:checked');
+    var resp3 = document.querySelector('input[name="quest_3"]:checked');
+    var resp4 = document.querySelector('input[name="quest_4"]:checked');
+
+    var contador = 0;
+
+
+    if(resp1 && resp2 && resp3 && resp4){
+        if(resp1.value == "correta"){
+            document.getElementById("resp_correta_1").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_1").innerHTML = "Errada, pois o tempo e a taxa estão em unidades de medida diferentes. Vamos transformar a taxa de 30% ao ano para uma taxa mensal. Como o ano possui 12 meses, então 30% : 12 = 2,5%.
+            "
+
+        }
+        if(resp2.value == "correta"){
+            document.getElementById("resp_correta_2").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_2").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+
+        }
+        if(resp3.value == "correta"){
+            document.getElementById("resp_correta_3").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_3").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+
+        }
+        if(resp4.value == "correta"){
+            document.getElementById("resp_correta_4").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_4").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+
+        }
+        document.getElementById("quantos_acertos").innerHTML = "Você acertou " + contador + " questões!";
+    }
+    else{
+        alert("não deixe questões em branco!")
+    }
+
 }
