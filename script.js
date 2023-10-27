6b1127b2fdad43f8629f6718e2f778a1aba3c878
staargaze.r <90391654+starryerik@users.noreply.github.com>
2023-10-27 08:03:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,11 +15,17 @@ function funcao_resposta(){
     var resp2 = document.querySelector('input[name="quest_2"]:checked');
     var resp3 = document.querySelector('input[name="quest_3"]:checked');
     var resp4 = document.querySelector('input[name="quest_4"]:checked');
+    var resp5 = document.querySelector('input[name="quest_5"]:checked');
+    var resp6 = document.querySelector('input[name="quest_6"]:checked');
+    var resp7 = document.querySelector('input[name="quest_7"]:checked');
+    var resp8 = document.querySelector('input[name="quest_8"]:checked');
+    var resp9 = document.querySelector('input[name="quest_9"]:checked');
+    var resp10 = document.querySelector('input[name="quest_10"]:checked');
 
     var contador = 0;
 
 
-    if(resp1 && resp2 && resp3 && resp4){
+    if(resp1 && resp2 && resp3 && resp4 && resp5 && resp6 && resp7 && resp8 && resp9 && resp10){
         if(resp1.value == "correta"){
             document.getElementById("resp_correta_1").innerHTML = "Correta!"
             contador = contador + 1;
@@ -50,6 +56,60 @@ function funcao_resposta(){
         }
         else{
             document.getElementById("resp_errada_4").innerHTML = "Errada, pois <img src='imagens/calculo.png'>"
+
+        }
+
+        if(resp5.value == "correta"){
+            document.getElementById("resp_correta_5").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_5").innerHTML = "Errada, pois <img src='imagens/calculo.png'>"
+
+        }
+
+        if(resp6.value == "correta"){
+            document.getElementById("resp_correta_6").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_6").innerHTML = "Errada, pois <img src='imagens/calculo.png'>"
+
+        }
+
+        if(resp7.value == "correta"){
+            document.getElementById("resp_correta_7").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_7").innerHTML = "Errada, pois <img src='imagens/calculo.png'>"
+
+        }
+
+        if(resp8.value == "correta"){
+            document.getElementById("resp_correta_8").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_8").innerHTML = "Errada, pois <img src='imagens/calculo.png'>"
+
+        }
+
+        if(resp9.value == "correta"){
+            document.getElementById("resp_correta_9").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_9").innerHTML = "Errada, pois <img src='imagens/calculo.png'>"
+
+        }
+
+        if(resp10.value == "correta"){
+            document.getElementById("resp_correta_10").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_10").innerHTML = "Errada, pois <img src='imagens/calculo.png'>"
 
         }
         document.getElementById("quantos_acertos").innerHTML = "Você acertou " + contador + " questões!";
